f7e185c29ce76d160c218e58d5834bb4c4633716
FIRE_EIX <632126598@qq.com>
2021-02-27 13:33:58 +0800

    2.27

--- 1304.cpp
+++ 1304.cpp
@@ -6,18 +6,34 @@
 
 using namespace std;
 
-bool isPrime(int a){
-    
+bool isPrime(int a) {
+    for (int i = 2; i * i <= a; i++) {
+        if (a % i == 0) return false;
+    }
+    return true;
+}
+
+void ans(int a) {
+    if (isPrime(a-2)) {
+        cout << a << "=" << "2+" << a-2 << endl;
+        return;
+    }
+    for (int i = 3; i + 3 <= a; i += 2) {
+        if (isPrime(i) && isPrime(a - i)) {
+            cout << a << "=" << min(i, a - i) << "+" << max(i, a - i) << endl;
+            return;
+        }
+    }
 }
 
 int main() {
     int n;
     cin >> n;
-    if (n == 4) {
-        cout << "4=2+2";
-    }else{
-        int
-        for(int i=3;i)
+    cout << "4=2+2" << endl;
+    if (n > 4) {
+        for (int i = 6; i <= n; i += 2) {
+            ans(i);
+        }
     }
     return 0;
 }
